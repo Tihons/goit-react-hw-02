e8b4cd4c458794dbed791ed244e72a030a704c63
Tihons <tihonshramenko@gmail.com>
2024-02-02 22:41:27 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import css from './App.module.css';
+import { useState, useEffect } from 'react';
+import { Description } from './Description/Description';
+import { Feedback } from './Feedback/Feedback';
+import { Options } from './Options/Options';
 
-function App() {
-  const [count, setCount] = useState(0)
+const config = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+const localStorage = () => {
+  const savedObject = window.localStorage.getItem('settings');
+  if (savedObject !== null) {
+    return JSON.parse(savedObject);
+  }
+  return config;
+};
+
+export const App = () => {
+  let [count, setCount] = useState(localStorage);
+
+  useEffect(() => {
+    window.localStorage.setItem('settings', JSON.stringify(count));
+  }, [count]);
+
+  const handleClick = value => {
+    setCount({ ...count, [value]: count[value] + 1 });
+  };
+
+  const handleClickReset = () => {
+    window.localStorage.clear();
+    setCount((count = config));
+  };
+
+  const nameParams = Object.keys(config);
+  const totalFeedback = count.good + count.bad + count.neutral;
+  const positive = Math.round(((count.good + count.neutral) / totalFeedback) * 100);
+  const cheakAnalogy = JSON.stringify(config) === JSON.stringify(count);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className={css.wrapper}>
+      <Description />
 
-export default App
+      <Options
+        checking={cheakAnalogy}
+        onHandleClick={handleClick}
+        onHandleClickReset={handleClickReset}
+        params={nameParams}
+      />
+
+      <Feedback checking={cheakAnalogy} total={totalFeedback} positive={positive} obj={count} />
+    </div>
+  );
+};
